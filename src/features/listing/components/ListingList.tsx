e9f731a3537d8afddac8f25a66ed7d61f909bdca
liljdot <jehuoki@gmail.com>
2025-07-12 19:04:32 +0100

    fix: update ListingList component to accept listings prop and render ListingCard

--- src/features/listing/components/ListingList.tsx
+++ src/features/listing/components/ListingList.tsx
@@ -1,14 +1,17 @@
 import type { Listing } from "../types";
+import ListingCard from "./ListingCard";
 
 interface Props {
     listings: Listing[]
 }
 
-const ListingList: React.FC<Props> = () => {
+const ListingList: React.FC<Props> = ({listings}) => {
 
     return (
         <>
-        
+         {
+            listings.map(listing => (<ListingCard listing={listing}/>))
+         }
         </>
     )
 }
