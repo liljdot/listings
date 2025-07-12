bada19b1b8e768a677bd2b1af05e5d3fc785fb9e
liljdot <jehuoki@gmail.com>
2025-07-12 19:06:00 +0100

    feat: render ListingList component in HomePage with empty listings prop

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,11 @@
+import ListingList from "@/features/listing/components/ListingList";
+
 const HomePage: React.FC = () => {
 
     return (
         <>
             <h1>HomePage</h1>
+            <ListingList listings={[]}/>
         </>
     )
 }
