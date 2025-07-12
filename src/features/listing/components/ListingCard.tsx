ac85248709e71df23cc3fb53a828d2bc6a03143b
liljdot <jehuoki@gmail.com>
2025-07-12 20:14:39 +0100

    define listingcard component and update listing type

--- src/features/listing/components/ListingCard.tsx
+++ src/features/listing/components/ListingCard.tsx
@@ -1,15 +1,26 @@
+import { Card, CardContent } from "@/components/ui/card";
 import type { Listing } from "../types";
+import { getImageUrl } from "@/lib/utils/images.js"
 
 interface Props {
     listing: Listing
 }
 
-const ListingCard: React.FC<Props> = () => {
+const ListingCard: React.FC<Props> = ({ listing }) => {
 
     return (
-        <>
-            <h3>Listing Card</h3>
-        </>
+        <Card className="w-[320px]">
+            <img
+                src={getImageUrl(listing.images[0])}
+                alt={listing.name}
+                className="w-full h-[200px] rounded-md object-cover"
+            />
+            <CardContent className="p-4">
+                <h2 className="mb-0 text-xl font-semibold">
+                    {listing.name}
+                </h2>
+            </CardContent>
+        </Card>
     )
 }
 
--- src/features/listing/types.ts
+++ src/features/listing/types.ts
@@ -1,3 +1,5 @@
 export interface Listing {
     id: number
+    name: string
+    images: string[]
 }
