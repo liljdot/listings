87a8bfdfd784b758244a6597240d4d269e3b97a7
liljdot <jehuoki@gmail.com>
2025-07-13 22:19:51 +0100

    create shadcn datepicker

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,5 +2,6 @@ export * from './Avatar';
 export * from "./button"
 export * from "./calendar"
 export * from './Card';
+export * from "./DatePicker";
 export * from './input';
 export * from "./popover"
