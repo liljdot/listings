99c4e2b1d04be92b0c239320363906bedf613e1e
liljdot <jehuoki@gmail.com>
2025-07-13 19:23:25 +0100

    convert listings variable to state

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,15 +2,17 @@ import ListingList from "@/features/listing/components/ListingList";
 // @ts-expect-error import from js file
 import { listings } from "@/api/data/listings.js"
 import type { Listing } from "@/features/listing/types";
+import { useState } from "react";
 
 const typedListings = listings as Listing[]
 
 const HomePage: React.FC = () => {
+    const [displayedListings, setDisplayedListings] = useState(typedListings)
 
     return (
         <>
             <div className="container py-4">
-                <ListingList listings={typedListings} />
+                <ListingList listings={displayedListings} />
             </div>
         </>
     )
