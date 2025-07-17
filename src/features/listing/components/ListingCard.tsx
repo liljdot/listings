689b49d25e94e6ffa34e72b2abecfd7835491e5d
liljdot <jehuoki@gmail.com>
2025-07-17 20:34:02 +0100

    feat: wrap ListingCard content in Link for improved navigation

--- src/features/listing/components/ListingCard.tsx
+++ src/features/listing/components/ListingCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardContent } from "@/components/ui";
 import type { ListingForList } from "../types";
 import { DollarSign, Pin, Users } from "lucide-react";
 import ListingCardImages from "./ListingCardImages";
+import { Link } from "react-router-dom";
 
 interface Props {
     listing: ListingForList
@@ -12,34 +13,36 @@ const ListingCard: React.FC<Props> = ({ listing }) => {
     return (
         <Card className="w-[320px]">
             <ListingCardImages listing={listing} />
-            <CardContent className="flex flex-col gap-2 p-4">
-                <h2 className="mb-2 text-xl font-semibold">
-                    {listing.name}
-                </h2>
-                <div className="flex items-center gap-2">
-                    <DollarSign className="h-4 w-4 text-primary" />
-                    <span className="text-muted-foreground">
-                        <span className="font-bold text-foreground">{listing.price}</span>/night
-                    </span>
-                </div>
-                <div className="flex items-center gap-2">
-                    <Pin className="h-4 w-4 text-primary" />
-                    <span className="text-muted-foreground">
-                        <span className="font-bold text-foreground">
-                            {listing.location.name}
+            <Link to={`/listing/${listing.id}`}>
+                <CardContent className="flex flex-col gap-2 p-4">
+                    <h2 className="mb-2 text-xl font-semibold">
+                        {listing.name}
+                    </h2>
+                    <div className="flex items-center gap-2">
+                        <DollarSign className="h-4 w-4 text-primary" />
+                        <span className="text-muted-foreground">
+                            <span className="font-bold text-foreground">{listing.price}</span>/night
                         </span>
-                    </span>
-                </div>
-                <div className="flex items-center gap-2">
-                    <Users className="h-4 w-4 text-primary" />
-                    <span className="text-muted-foreground">
-                        <span className="font-bold text-foreground">
-                            {listing.maxGuests} Guests
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <Pin className="h-4 w-4 text-primary" />
+                        <span className="text-muted-foreground">
+                            <span className="font-bold text-foreground">
+                                {listing.location.name}
+                            </span>
                         </span>
-                    </span>
-                </div>
-            </CardContent>
-        </Card>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <Users className="h-4 w-4 text-primary" />
+                        <span className="text-muted-foreground">
+                            <span className="font-bold text-foreground">
+                                {listing.maxGuests} Guests
+                            </span>
+                        </span>
+                    </div>
+                </CardContent>
+            </Link >
+        </Card >
     )
 }
 
