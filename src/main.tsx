590172952ed7dc3f9c27cc22f5646721d582bd8c
liljdot <jehuoki@gmail.com>
2025-07-17 19:20:19 +0100

    feat: initialize local database with seed data in main entry

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Router from './Router.tsx'
+// @ts-expect-error import from js file
+import { seedLocalDatabase } from '@/api/data/seed';
+
+seedLocalDatabase()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
