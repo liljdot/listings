d2c946eed481460efdd20718f577d5d3d87b6b59
liljdot <jehuoki@gmail.com>
2025-07-17 19:02:49 +0100

    feat: add react-router-dom for routing and create Router component

---
+++ src/Router.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+const Router: React.FC = () => {
+    const router = createBrowserRouter([
+
+    ])
+
+    return (
+        <>
+            <RouterProvider router={router} />
+        </>
+    )
+}
+
+export default Router;
