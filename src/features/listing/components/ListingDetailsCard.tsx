e14692b23313e187efea69e03afcff3325b957c4
liljdot <jehuoki@gmail.com>
2025-07-17 19:24:33 +0100

    feat: add ListingDetailsCard component for displaying listing details

---
+++ src/features/listing/components/ListingDetailsCard.tsx
@@ -0,0 +1,16 @@
+import type { ListingForList } from "../types"
+
+interface Props {
+    listing: ListingForList
+}
+
+const ListingDetailsCard: React.FC<Props> = ({ listing }) => {
+
+    return (
+        <>
+            Listing Details Card
+        </>
+    )
+}
+
+export default ListingDetailsCard;
