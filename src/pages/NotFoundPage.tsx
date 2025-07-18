9dfcf0282655c117065eda4e6073de58c52a21b3
liljdot <jehuoki@gmail.com>
2025-07-18 16:49:43 +0100

    feat: implement NotFoundPage with user-friendly 404 message and navigation

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,8 +1,28 @@
+import { Button, Card } from "@/components/ui";
+import { Link } from "react-router-dom";
+
 const NotFoundPage: React.FC = () => {
 
     return (
         <>
-            NotFound
+            <div className="container flex h-screen w-screen items-center justify-center py-4 text-center">
+                <Card className="p-8">
+                    <h1>
+                        Page not found
+                    </h1>
+                    <p className="pb-2">
+                        Unfortunately, the page that you're looking for does not exist.
+                    </p>
+                    <Button asChild>
+                        <Link
+                            to={"/"}
+                            replace
+                        >
+                            Back to Home
+                        </Link>
+                    </Button>
+                </Card>
+            </div>
         </>
     )
 }
