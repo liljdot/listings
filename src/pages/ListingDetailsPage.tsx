9f0c105713bd5801f50bcb8e5cc01e6640273790
liljdot <jehuoki@gmail.com>
2025-07-19 21:57:39 +0100

    refactor: replace manual loading and error handling with DataRenderer in ListingDetailsPage

--- src/pages/ListingDetailsPage.tsx
+++ src/pages/ListingDetailsPage.tsx
@@ -1,42 +1,23 @@
 import { useParams } from "react-router-dom";
 import ListingDetailsCard from "@/features/listing/components/ListingDetailsCard";
-import { Spinner } from "@/components/ui";
 import useFetch from "@/features/listing/hooks/useFetch";
 import type { ListingForList } from "@/features/listing/types";
+import DataRenderer from "@/features/shared/DataRenderer";
 
 const ListingDetailsPage: React.FC = () => {
     const { listingId } = useParams()
 
     const { data: listing, isLoading, error } = useFetch<ListingForList>(`/api/listings/${listingId}`, undefined, [listingId])
 
-    const renderListingDetails = () => {
-        if (isLoading) {
-            return (
-                <div className="flex justify-center">
-                    <Spinner size={"sm"} />
-                </div>
-            );
-        }
-
-        if (error) {
-            return (
-                <div className="text-center">
-                    {error}
-                </div>
-            );
-        }
-
-        return (
-            <ListingDetailsCard listing={listing!} />
-        )
-    }
-
     return (
         <>
             <div className="container py-4">
-                {
-                    renderListingDetails()
-                }
+                <DataRenderer
+                    isLoading={isLoading}
+                    error={error}
+                >
+                    <ListingDetailsCard listing={listing!} />
+                </DataRenderer>
             </div>
         </>
     )
