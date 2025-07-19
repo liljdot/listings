b083dbcce59fe5e8290001e7182d260fa415e5ff
liljdot <jehuoki@gmail.com>
2025-07-19 20:35:21 +0100

    refactor: consolidate rendering logic for listing details into a separate function

--- src/pages/ListingDetailsPage.tsx
+++ src/pages/ListingDetailsPage.tsx
@@ -9,31 +9,33 @@ const ListingDetailsPage: React.FC = () => {
 
     const { data: listing, isLoading, error } = useFetch<ListingForList>(`/api/listings/${listingId}`, undefined, [listingId])
 
+    const renderListingDetails = () => {
+        if (isLoading) {
+            return (
+                <div className="flex justify-center">
+                    <Spinner size={"sm"} />
+                </div>
+            );
+        }
+
+        if (error) {
+            return (
+                <div className="text-center">
+                    {error}
+                </div>
+            );
+        }
+
+        return (
+            <ListingDetailsCard listing={listing!} />
+        )
+    }
 
     return (
         <>
             <div className="container py-4">
                 {
-                    isLoading && (
-                        <div className="flex justify-center">
-                            <Spinner size={"sm"} />
-                        </div>
-                    )
-                }
-
-                {
-                    error && (
-                        <div className="text-center">
-                            {error}
-                        </div>
-                    )
-                }
-
-                {
-                    !isLoading &&
-                    !error && (
-                        <ListingDetailsCard listing={listing!} />
-                    )
+                    renderListingDetails()
                 }
             </div>
         </>
