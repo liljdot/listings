ef14cb21464008044c86e51092defdfacf960388
liljdot <jehuoki@gmail.com>
2025-07-20 20:56:02 +0100

    refactor: enhance useFetch hook with caching and storage management

--- src/features/listing/hooks/useFetch.ts
+++ src/features/listing/hooks/useFetch.ts
@@ -1,12 +1,28 @@
 import { useEffect, useRef, useState } from "react"
 // @ts-expect-error import from js file
 import api from "@/api"
+// @ts-expect-error import from js file
+import { getItem, setItem } from "@/lib/utils/localStorage"
 import type { AxiosInstance } from "axios"
 import axios from "axios"
 
 const typedApi = api as AxiosInstance
+const typedGetItem = getItem as <T>(key: string) => { lastFetched: number, data: T } | undefined
+const typedSetItem = setItem as <T>(key: string, value: { lastFetched: number, data: T }) => void
 
 const useFetch = <T>(path: string, params?: unknown, deps?: unknown[]) => {
+    const STALE_TIME = 1000 * 60 * 5 // 5 minutes
+
+    const getStorageKey = () => {
+        if (!params) {
+            return path
+        }
+
+        return `${path}?${JSON.stringify(params)}`
+    }
+
+    const storageKey = getStorageKey()
+
     const [isLoading, setIsLoading] = useState(true)
     const [error, setError] = useState<string | null>(null)
 
@@ -15,6 +31,17 @@ const useFetch = <T>(path: string, params?: unknown, deps?: unknown[]) => {
     const abortController = useRef<AbortController>(null)
 
     useEffect(() => {
+        const now = new Date().getTime()
+        const cachedData = typedGetItem<T>(storageKey)
+
+        if (cachedData &&
+            now - cachedData.lastFetched < STALE_TIME
+        ) {
+            setData(cachedData.data)
+            setIsLoading(false)
+            return
+        }
+
         abortController.current = new AbortController()
 
         const fetchData = () => {
@@ -27,9 +54,6 @@ const useFetch = <T>(path: string, params?: unknown, deps?: unknown[]) => {
             })
                 .then(res => {
                     setData(res.data)
-                    // if (Math.random() > 0.8) {
-                    //     throw new Error("Simulated fetch error")
-                    // }
                 })
                 .catch((err) => {
                     if (axios.isCancel(err)) {
@@ -51,6 +75,17 @@ const useFetch = <T>(path: string, params?: unknown, deps?: unknown[]) => {
         path,
         params
     ])
+
+    useEffect(() => {
+        if (!data) {
+            return
+        }
+
+        typedSetItem(storageKey, {
+            lastFetched: new Date().getTime(),
+            data
+        })
+    }, [data, storageKey])
 
     return {
         isLoading,
