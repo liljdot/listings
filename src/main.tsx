8e15b3a3f3d7d46070a927a8851604cacf7b37ad
liljdot <jehuoki@gmail.com>
2025-07-22 20:46:10 +0100

    refactor: replace StrictMode with Redux Provider in main.tsx and add store configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Router from './Router.tsx'
 // @ts-expect-error import from js file
 import { seedLocalDatabase } from '@/api/data/seed';
+import { Provider } from 'react-redux';
+import store from './state/store.ts';
 
 seedLocalDatabase()
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <Provider store={store}>
     <Router />
-  </StrictMode>,
+  </Provider >,
 )
