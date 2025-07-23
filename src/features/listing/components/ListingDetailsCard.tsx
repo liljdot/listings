460baa87d8984dfe5c4e96582546149da9ec8114
liljdot <jehuoki@gmail.com>
2025-07-23 21:18:34 +0100

    add listingfavoritebutton to listingdetailspage

--- src/features/listing/components/ListingDetailsCard.tsx
+++ src/features/listing/components/ListingDetailsCard.tsx
@@ -2,6 +2,7 @@ import { Card, Separator } from "@/components/ui";
 import type { ListingForList } from "../types"
 import { DollarSign, Pin, Users } from "lucide-react";
 import ListingDetailsCardImages from "./ListingDetailsCardImages";
+import ListingFavoriteButton from "./ListingFavoriteButton";
 
 interface Props {
     listing: ListingForList
@@ -14,30 +15,33 @@ const ListingDetailsCard: React.FC<Props> = ({ listing }) => {
             <Card className="mx-auto p-4">
                 <ListingDetailsCardImages listing={listing} />
                 <Separator className="mb-4" />
-                <div className='flex flex-col gap-2'>
-                    <h1 className="mb-2 text-2xl font-bold">
-                        {listing.name}
-                    </h1>
-                    <div className="flex items-center gap-2">
-                        <DollarSign className="h-4 w-4 text-primary" />
-                        <span className="text-muted-foreground">
-                            <span className="font-bold text-foreground">
-                                {listing.price} / night
+                <div className="flex justify-between">
+                    <div className='flex flex-col gap-2'>
+                        <h1 className="mb-2 text-2xl font-bold">
+                            {listing.name}
+                        </h1>
+                        <div className="flex items-center gap-2">
+                            <DollarSign className="h-4 w-4 text-primary" />
+                            <span className="text-muted-foreground">
+                                <span className="font-bold text-foreground">
+                                    {listing.price} / night
+                                </span>
                             </span>
-                        </span>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <Pin className="h-4 w-4 text-primary" />
+                            <span className='text-muted-foreground'>
+                                {listing.location.name}
+                            </span>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <Users className="h-4 w-4 text-primary" />
+                            <span className='text-muted-foreground'>
+                                {listing.maxGuests} Guests
+                            </span>
+                        </div>
                     </div>
-                    <div className="flex items-center gap-2">
-                        <Pin className="h-4 w-4 text-primary" />
-                        <span className='text-muted-foreground'>
-                            {listing.location.name}
-                        </span>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Users className="h-4 w-4 text-primary" />
-                        <span className='text-muted-foreground'>
-                            {listing.maxGuests} Guests
-                        </span>
-                    </div>
+                    <ListingFavoriteButton listingId={listing.id} />
                 </div>
                 <Separator className="my-4" />
                 <div className="whitespace-pre-line">
