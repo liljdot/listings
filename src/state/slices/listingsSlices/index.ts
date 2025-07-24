33be9f77e63005857f0b1642a7ab686411da1e03
liljdot <jehuoki@gmail.com>
2025-07-24 18:53:39 +0100

    implement caching support in fetchlistings thunk

--- src/state/slices/listingsSlices/index.ts
+++ src/state/slices/listingsSlices/index.ts
@@ -3,10 +3,14 @@ import { createAsyncThunk, createSlice, type PayloadAction } from "@reduxjs/tool
 import type { DateRange } from "react-day-picker";
 // @ts-expect-error import from js file
 import api from "@/api"
+// @ts-expect-error import from js file
+import { getItem, setItem } from "@/lib/utils/localStorage"
 import type { AxiosInstance } from "axios";
 import axios from "axios";
 
 const typedApi = api as AxiosInstance
+const typedGetItem = getItem as <T>(key: string) => { lastFetched: number, data: T } | undefined
+const typedSetItem = setItem as <T>(key: string, value: { lastFetched: number, data: T }) => void
 
 const initialState: {
     listings: ListingForList[],
@@ -74,10 +78,43 @@ const listingsSlice = createSlice({
 })
 
 export const fetchListings = createAsyncThunk("listings/fetchListings", (filters?: { search: string, guests: number, dates?: DateRange }) => {
-    return typedApi.get<ListingForList[]>("/api/listings", {
-        params: filters
+    const STALE_TIME = 1000 * 60 * 5 // 5 minutes
+    const storageKey = (() => {
+        if (!filters) {
+            return "/api/listings"
+        }
+
+        return `${"/api/listings"}?${JSON.stringify(filters)}`
+    })()
+
+
+    return new Promise<ListingForList[]>((resolve, reject) => {
+        const now = new Date().getTime()
+        const cachedData = typedGetItem<ListingForList[]>(storageKey) // retrieve cached data from localStorage
+
+        if (cachedData &&
+            now - cachedData.lastFetched < STALE_TIME // check if the cached data is still fresh based on the STALE_TIME
+        ) {
+             resolve(cachedData.data)
+        } // if cached data exists and is fresh, set the data and loading state and return early
+
+        typedApi.get<ListingForList[]>("/api/listings", {
+            params: filters
+        })
+            .then(res => {
+                const data = res.data
+
+                typedSetItem<ListingForList[]>(storageKey, {
+                    lastFetched: now,
+                    data
+                })
+
+                resolve(data)
+            })
+            .catch(err => {
+                reject(err)
+            })
     })
-        .then(res => res.data)
 })
 
 export const { addFavoriteListing, removeFavoriteListing } = listingsSlice.actions
