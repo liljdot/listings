e2f62c6d0b43a0cba1012da3ca7d7fa92ce8f935
liljdot <jehuoki@gmail.com>
2025-07-25 21:24:50 +0100

    plug sign in form to sign in page

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,8 +1,12 @@
+import SignInForm from "@/features/shared/auth/components/SignInForm";
+
 const SignInPage: React.FC = () => {
     return (
-        <div className='container flex h-screen items-center justify-center py-4'>
-            Sign in form goes here!
-        </div>
+        <>
+            <div className='container flex h-screen items-center justify-center py-4'>
+                <SignInForm />
+            </div>
+        </>
     );
 };
 
