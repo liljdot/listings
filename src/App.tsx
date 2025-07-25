1b3442b549001e9be66fe60c1ddc85e411727d59
liljdot <jehuoki@gmail.com>
2025-07-25 20:44:23 +0100

    render  navbar on auth condition

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom"
 import NavBar from "./features/shared/components/Navbar"
+import { useAuthContext } from "./features/shared/auth/contexts/AuthProvider"
 
 function App() {
+  const { token } = useAuthContext()
 
   return (
     <>
-      <NavBar />
+      {
+        token && <NavBar />
+      }
       <Outlet />
     </>
   )
