d60c83144f3b01e8cb5a5da690299efc575a0791
liljdot <jehuoki@gmail.com>
2025-07-28 20:20:05 +0100

    implement signout feature

--- src/features/shared/components/Navbar.tsx
+++ src/features/shared/components/Navbar.tsx
@@ -1,17 +1,45 @@
-import { Separator } from "@/components/ui";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger, Separator } from "@/components/ui";
 import { Link } from "react-router-dom";
+import { useAuthContext } from "../auth/contexts/AuthProvider";
+// @ts-expect-error import from js file
+import api from "@/api";
+import type { AxiosInstance } from "axios";
+
+const typedApi = api as AxiosInstance
 
 const NavBar: React.FC = () => {
+    const { setToken } = useAuthContext()
+
+    const handleSignOut = () => {
+        typedApi.post("/api/signout")
+            .finally(() => {
+                setToken(null)
+            })
+    }
 
     return (
         <>
-            <div className="flex flex-row justify-center gap-8 px-8 py-4">
+            <div className="flex flex-row items-center justify-between gap-8 px-8 py-4">
                 <Link to={"/"}>
                     Home
                 </Link>
-                <Link to={"/favorites"}>
-                    Favorites
-                </Link>
+                <div className="flex-end flex flex-row items-center gap-8">
+                    <Link to={"/favorites"}>
+                        Favorites
+                    </Link>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Link to={""}>
+                                Account
+                            </Link>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuItem onClick={handleSignOut}>
+                                Sign Out
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
             </div>
             <Separator />
         </>
