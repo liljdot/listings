cfea474588ec27f6d13ffd6f494a4dbc271a1d75
liljdot <jehuoki@gmail.com>
2025-07-31 18:14:10 +0100

    try to fix import resolution

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -6,7 +6,7 @@ export * from "./Carousel"
 export * from "./DatePicker";
 export * from "./DropdownMenu"
 export * from './Input';
-export * from "./Popover"
+export * from "./Popover" 
 export * from "./Separator"
 export * from "./Spinner"
 export * from "./Stepper"
