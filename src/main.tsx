4db71899b645d39d475dac5ff6aba624bdb2128f
liljdot <jehuoki@gmail.com>
2025-08-02 18:30:13 +0100

    add queryCLientProvider to topm of tree

--- src/main.tsx
+++ src/main.tsx
@@ -7,15 +7,20 @@ import { Provider } from 'react-redux';
 import store from './state/store.ts';
 import AuthProvider from './features/shared/auth/contexts/AuthProvider.tsx';
 import ThemeProvider from './features/shared/theme/contexts/ThemeProvider.tsx';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 seedLocalDatabase()
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <ThemeProvider>
-      <AuthProvider>
-        <Router />
-      </AuthProvider>
-    </ThemeProvider>
-  </Provider >,
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <ThemeProvider>
+        <AuthProvider>
+          <Router />
+        </AuthProvider>
+      </ThemeProvider>
+    </Provider >
+  </QueryClientProvider>,
 )
