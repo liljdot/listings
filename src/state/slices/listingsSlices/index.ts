2e8470d027adbf5de0503300ac329933567cfef2
liljdot <jehuoki@gmail.com>
2025-08-03 21:27:08 +0100

    add matchers for new getListings query

--- src/state/slices/listingsSlices/index.ts
+++ src/state/slices/listingsSlices/index.ts
@@ -75,6 +75,27 @@ const listingsSlice = createSlice({
                 error: action.error.message || "Something went wrong while fetching listings"
             }
         })
+        builder.addMatcher(listingsApi.endpoints.getListings.matchPending, state => ({
+            ...state,
+            isLoading: true,
+            error: null
+        }));
+        builder.addMatcher(listingsApi.endpoints.getListings.matchFulfilled, (state, action) => ({
+            ...state,
+            isLoading: false,
+            listings: action.payload
+        }));
+        builder.addMatcher(listingsApi.endpoints.getListings.matchRejected, (state, action) => {
+            if (axios.isCancel(action.error)) {
+                return
+            }
+
+            return {
+                ...state,
+                isLoading: false,
+                error: action.error.message || "Something went wrong while fetching listings"
+            }
+        })
         builder.addMatcher(listingsApi.endpoints.getSingleListing.matchFulfilled, (state, response) => {
             console.log("i have fetched a single listing:", response)
         })
@@ -102,7 +123,7 @@ export const fetchListings = createAsyncThunk("listings/fetchListings", (filters
         if (cachedData &&
             now - cachedData.lastFetched < STALE_TIME // check if the cached data is still fresh based on the STALE_TIME
         ) {
-             resolve(cachedData.data)
+            resolve(cachedData.data)
         } // if cached data exists and is fresh, set the data and loading state and return early
 
         typedApi.get<ListingForList[]>("/api/listings", {
