b2f8f5bc1c51127d93c7ce4ea881e853fb610c2c
liljdot <jehuoki@gmail.com>
2025-08-03 22:26:34 +0100

    implement  rtk getsinglelisting in listing details page

--- src/pages/ListingDetailsPage.tsx
+++ src/pages/ListingDetailsPage.tsx
@@ -1,20 +1,19 @@
 import { useParams } from "react-router-dom";
 import ListingDetailsCard from "@/features/listing/components/ListingDetailsCard";
-import useFetch from "@/features/listing/hooks/useFetch";
-import type { ListingForList } from "@/features/listing/types";
 import DataRenderer from "@/features/shared/components/DataRenderer";
+import { useGetSingleListingQuery } from "@/services/api/listingsApi";
 
 const ListingDetailsPage: React.FC = () => {
     const { listingId } = useParams()
 
-    const { data: listing, isLoading, error } = useFetch<ListingForList>(`/api/listings/${listingId}`, undefined, [listingId])
+    const { data: listing, isLoading, error } = useGetSingleListingQuery(Number(listingId))
 
     return (
         <>
             <div className="container py-4">
                 <DataRenderer
                     isLoading={isLoading}
-                    error={error}
+                    error={error as string}
                 >
                     <ListingDetailsCard listing={listing!} />
                 </DataRenderer>
