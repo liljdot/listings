d35bf1f2cee59f10b15d505c4d5df83415608c22
liljdot <jehuoki@gmail.com>
2025-08-04 19:20:40 +0100

    fix: implement rtk query in listings favorites page as favorites could only be a subset of listings array in listings slice

--- src/pages/ListingFavoritesPage.tsx
+++ src/pages/ListingFavoritesPage.tsx
@@ -1,19 +1,33 @@
 import ListingList from "@/features/listing/components/ListingList";
+import DataRenderer from "@/features/shared/components/DataRenderer";
+import { useGetListingsQuery } from "@/services/api/listingsApi";
 import type { RootState } from "@/state/store";
+import type { AxiosError } from "axios";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 
 const ListingFavoritesPage: React.FC = () => {
-    const { favoritesListingIds, listings } = useSelector((state: RootState) => state.listings)
+    const { favoritesListingIds } = useSelector((state: RootState) => state.listings)
+
+    const { data: listings, isLoading, error } = useGetListingsQuery(undefined)
 
     const favoriteListings = useMemo(() => {
+        if (!listings) {
+            return []
+        }
+
         return listings.filter(listing => favoritesListingIds.includes(listing.id))
     }, [favoritesListingIds, listings])
 
     return (
         <>
             <div className="container py-4">
-                <ListingList listings={favoriteListings} />
+                <DataRenderer
+                    isLoading={isLoading}
+                    error={error ? (error as AxiosError).message : null}
+                >
+                    <ListingList listings={favoriteListings} />
+                </DataRenderer>
             </div>
         </>
     )
