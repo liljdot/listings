e0375966b6c9e82cf7eea7090d3105d92c06f7a3
liljdot <jehuoki@gmail.com>
2025-08-07 17:21:00 +0100

    create userAvatar component

---
+++ src/features/user/components/UserAvatar.tsx
@@ -0,0 +1,18 @@
+import type { User } from "../types"
+
+interface Props {
+    className: string
+    imageOnly: boolean
+    user: User
+}
+
+const UserAvater: React.FC<Props> = ({className, imageOnly, user}) => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default UserAvater
