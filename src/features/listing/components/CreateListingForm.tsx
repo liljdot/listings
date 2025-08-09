ba458fc2fe5e7fef4d87edf636d964674c402117
liljdot <jehuoki@gmail.com>
2025-08-09 18:24:14 +0100

    create createlistingForm with schema

---
+++ src/features/listing/components/CreateListingForm.tsx
@@ -0,0 +1,36 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+const createListingFormSchema = z.object({
+    name: z.string(),
+    description: z.string().optional(),
+    locationId: z.coerce.number(),
+    images: z.array(z.string()).min(1),
+    price: z.coerce.number({
+        error: "Price must be a whole number"
+    })
+    .min(1),
+    maxGuests: z.number().min(1),
+    availability: z.object({
+        from: z.date(),
+        to: z.date()
+    })
+})
+
+const CreateListingForm: React.FC = () => {
+    const form = useForm({
+        resolver: zodResolver(createListingFormSchema),
+        defaultValues: {
+            maxGuests: 1
+        }
+    })
+
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default CreateListingForm;
