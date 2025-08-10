0a69177218f56cb89e46fec574a895362804d6cb
liljdot <jehuoki@gmail.com>
2025-08-10 15:57:00 +0100

    implement SelectInput in CreateListingForm

--- src/features/listing/components/CreateListingForm.tsx
+++ src/features/listing/components/CreateListingForm.tsx
@@ -1,6 +1,11 @@
+import { Card, CardContent, CardHeader, Separator } from "@/components/ui";
+import Form from "@/components/ui/Form";
+import SelectInput from "@/components/ui/SelectInput";
+import TextInput from "@/components/ui/TextInput";
 import { useCreateListingMutation } from "@/services/api/listingsApi";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import z from "zod";
 
 const createListingFormSchema = z.object({
@@ -11,7 +16,7 @@ const createListingFormSchema = z.object({
     price: z.coerce.number({
         error: "Price must be a whole number"
     })
-    .min(1),
+        .min(1),
     maxGuests: z.number().min(1),
     availability: z.object({
         from: z.date(),
@@ -24,6 +29,8 @@ export type CreateListingFormSchemaType = z.infer<typeof createListingFormSchema
 const CreateListingForm: React.FC = () => {
     const [mutate] = useCreateListingMutation()
 
+    const navigate = useNavigate()
+
     const form = useForm({
         resolver: zodResolver(createListingFormSchema),
         defaultValues: {
@@ -33,11 +40,57 @@ const CreateListingForm: React.FC = () => {
 
     const onSubmit = form.handleSubmit(data => {
         return mutate(data).unwrap()
+            .then(res => navigate(`/listings/${res.id}`))
+            .catch((err: string) => {
+                console.log(err)
+                form.setError("root", {
+                    message: err
+                })
+            })
     })
+
+    const locationOptions = [
+        {value: "1", label: "Paris"},
+        {value: "2", label: "London"}
+    ]
 
     return (
         <>
-        
+            <Card className="mx-auto w-[800px]">
+                <CardHeader>
+                    <h2 className="text-center text-2xl">
+                        Create Listing
+                    </h2>
+                    <p className="text-center text-muted-foreground">
+                        Create a new listing
+                    </p>
+                    <Separator />
+                </CardHeader>
+                <CardContent>
+                    <Form
+                        form={form}
+                        onSubmit={onSubmit}
+                    >
+                        <TextInput
+                            control={form.control}
+                            name="name"
+                            placeholder="Listing name"
+                        />
+                        <TextInput
+                            control={form.control}
+                            aria-multiline
+                            name={"description"}
+                            placeholder="Description"
+                        />
+                        <SelectInput
+                            control={form.control}
+                            name={"locationId"}
+                            options={locationOptions}
+                            placeholder="Select a location"
+                        />
+                    </Form>
+                </CardContent>
+            </Card>
         </>
     )
 }
