d73acc6c7f79703e91cd9cffda3fb41aec6169bb
liljdot <jehuoki@gmail.com>
2025-08-11 20:56:45 +0100

    create stepperinput custom component from shadcn stepper

---
+++ src/components/ui/StepperInput.tsx
@@ -0,0 +1,36 @@
+import { useController, type Control, type FieldPath, type FieldValues } from "react-hook-form";
+import { Stepper, type StepperProps } from "./Stepper";
+
+interface Props<T extends FieldValues> extends StepperProps {
+    control: Control<T>
+    name: FieldPath<T>
+}
+
+const StepperInput = <T extends FieldValues>({
+    control,
+    name,
+    ...props
+}: Props<T>) => {
+    const form = useController({ control, name })
+
+    return (
+        <>
+            <div className="flex flex-col gap-2">
+                <Stepper
+                    {...props}
+                    value={form.field.value || 0}
+                    onChange={form.field.onChange}
+                />
+                {
+                    form.fieldState.error && (
+                        <div className="text-sm text-red-500">
+                            {form.fieldState.error.message}
+                        </div>
+                    )
+                }
+            </div>
+        </>
+    )
+}
+
+export default StepperInput
