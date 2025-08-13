5addc41be3e09bda7bbd8fb52f1553fc4e909291
liljdot <jehuoki@gmail.com>
2025-08-13 18:43:44 +0100

    add createListingForm to createListingPage

--- src/pages/CreateListingPage.tsx
+++ src/pages/CreateListingPage.tsx
@@ -1,9 +1,11 @@
+import CreateListingForm from "@/features/listing/components/CreateListingForm";
+
 const CreateListingPage: React.FC = () => {
 
     return (
         <>
             <div className="container py-4">
-
+                <CreateListingForm />
             </div>
         </>
     )
