4d8f2af4b5ee01b1242862876bf0acb1b668340c
liljdot <jehuoki@gmail.com>
2025-08-14 18:58:50 +0100

    fetch and display reviews in listingdetailspage

--- src/pages/ListingDetailsPage.tsx
+++ src/pages/ListingDetailsPage.tsx
@@ -3,21 +3,40 @@ import ListingDetailsCard from "@/features/listing/components/ListingDetailsCard
 import DataRenderer from "@/features/shared/components/DataRenderer";
 import { useGetSingleListingQuery } from "@/services/api/listingsApi";
 import type { AxiosError } from "axios";
+import { useGetListingReviewsQuery } from "@/services/api/reviewsApi";
+import ReviewsList from "@/features/review/components/ReviewsList";
 
 const ListingDetailsPage: React.FC = () => {
     const { listingId } = useParams()
 
     const { data: listing, isLoading, error } = useGetSingleListingQuery(Number(listingId))
 
+    const { data: reviews = [], isLoading: getReviewsIsLoading, error: getReviewsError } = useGetListingReviewsQuery(Number(listingId))
+
     return (
         <>
             <div className="container py-4">
-                <DataRenderer
-                    isLoading={isLoading}
-                    error={error ? (error as AxiosError).message : null}
-                >
-                    <ListingDetailsCard listing={listing!} />
-                </DataRenderer>
+                <div className="mb-8">
+                    <DataRenderer
+                        isLoading={isLoading}
+                        error={error ? (error as AxiosError).message : null}
+                    >
+                        <ListingDetailsCard listing={listing!} />
+                    </DataRenderer>
+                    {
+                        listing && (
+                            <div>
+                                <h2 className="mb-4">Reviews</h2>
+                                <DataRenderer
+                                    isLoading={getReviewsIsLoading}
+                                    error={getReviewsError ? (getReviewsError as AxiosError).message : null}
+                                >
+                                    <ReviewsList reviews={reviews} />
+                                </DataRenderer>
+                            </div>
+                        )
+                    }
+                </div>
             </div>
         </>
     )
