d161b7bde5842790ba874b9295c2e5d4d445670b
liljdot <jehuoki@gmail.com>
2025-08-14 18:22:53 +0100

    create stars component for review card

---
+++ src/features/review/components/ReviewCardStars.tsx
@@ -0,0 +1,34 @@
+import { cn } from "@/lib/utils";
+import type { ReviewForList } from "../types";
+import { Star } from "lucide-react";
+
+interface Props {
+    review: ReviewForList
+    className?: string
+}
+
+const ReviewCardStars: React.FC<Props> = ({ review, className }) => {
+
+    return (
+        <>
+            <div className={cn(
+                "inline-flex flex-row items-center",
+                className
+            )}>
+                {
+                    [...Array(5).map((...[, index]) => (
+                        <Star
+                            key={index}
+                            className={cn(
+                                "h-5 w-5 fill-secondary text-secondary",
+                                { "fill-star text-star": index < review.rating }
+                            )}
+                        />
+                    ))]
+                }
+            </div>
+        </>
+    )
+}
+
+export default ReviewCardStars;
