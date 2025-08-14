e955de1d27333cb4c61a10459b0dd3de6abcfd64
liljdot <jehuoki@gmail.com>
2025-08-14 17:44:27 +0100

    create reviewsApi

---
+++ src/services/api/reviewsApi.ts
@@ -0,0 +1,34 @@
+import type { Listing } from "@/features/listing/types";
+import { createApi, type BaseQueryFn } from "@reduxjs/toolkit/query/react";
+// @ts-expect-error import from js file
+import api from "@/api"
+import type { AxiosInstance } from "axios";
+import type { Review, ReviewForList } from "@/features/review/types";
+
+const typedApi = api as AxiosInstance
+
+const customBaseQuery: BaseQueryFn<Listing["id"], unknown, unknown> = (data) => {
+
+    return typedApi.get<Review[]>("/api/reviews", {
+        params: { listingId: data }
+    })
+}
+
+const reviewsApi = createApi({
+    reducerPath: "api/reviewsApi",
+    baseQuery: customBaseQuery,
+    tagTypes: ["Reviews"],
+    endpoints: builder => ({
+        getListingReviews: builder.query<ReviewForList[], Listing["id"]>({
+            query: data => data,
+            providesTags: (...[, , arg]) => [
+                {
+                    type: "Reviews",
+                    id: arg
+                }
+            ]
+        })
+    })
+})
+
+export const { useGetListingReviewsQuery } = reviewsApi
