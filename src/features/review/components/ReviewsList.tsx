c24e92c1b90a8e95227b5e7b1eb0fa24c64741df
liljdot <jehuoki@gmail.com>
2025-08-14 18:06:08 +0100

    create reviewsList component

---
+++ src/features/review/components/ReviewsList.tsx
@@ -0,0 +1,22 @@
+import type { Review } from "../types";
+
+interface Props {
+    reviews: Review[]
+}
+
+const ReviewsList: React.FC<Props> = ({ reviews }) => {
+
+    return (
+        <>
+            <div className="flex flex-col gap-4">
+                {
+                    reviews.length
+                        ? reviews.map(review => ())
+                        : <p>No reviews found.</p>
+                }
+            </div>
+        </>
+    )
+}
+
+export default ReviewsList
