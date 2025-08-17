00c17ad2753abf5fa8374bbf0aa9df895bb17a06
liljdot <jehuoki@gmail.com>
2025-08-17 21:05:37 +0100

    cy test homepage exists and renders

--- cypress/e2e/homePage.cy.ts
+++ cypress/e2e/homePage.cy.ts
@@ -1,5 +1,13 @@
 describe('Home Page', () => {
+  beforeEach(() => {
+    cy.session("user", () => {
+      cy.login("jehu@demo.io", "password")
+    })
+
+    cy.visit("http://localhost:5173/") // visit the homepage
+  }) // login and save session before  each test
+
   it('renders homepage with expected data', () => {
-    cy.login("jehu@demo.io", "password")
+    cy.get('[data-testid="homepage"]').should("exist")
   })
 });
