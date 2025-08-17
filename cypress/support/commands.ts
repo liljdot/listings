a0abccd7d88b1188ec9134a79b432bc42b9bff0a
liljdot <jehuoki@gmail.com>
2025-08-17 20:50:05 +0100

    add login to cypress type namespace

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -11,7 +11,7 @@
 //
 //
 // -- This is a parent command --
-Cypress.Commands.add('login', (email: string, password: string) => {
+Cypress.Commands.add("login", (email: string, password: string) => {
     cy.visit("http://localhost:5173/signin")
 
     cy.get("input[name='email']").type(email)
