5a35bad02b7a44e8ca5e40589f839c66877d1d2a
liljdot <jehuoki@gmail.com>
2025-08-18 19:31:03 +0100

    update homepage test to check for expected elements

--- cypress/e2e/homePage.cy.ts
+++ cypress/e2e/homePage.cy.ts
@@ -7,7 +7,9 @@ describe('Home Page', () => {
     cy.visit("http://localhost:5173/") // visit the homepage
   }) // login and save session before  each test
 
-  it('renders homepage with expected data', () => {
+  it('renders homepage with expected elements', () => {
     cy.get('[data-testid="homepage"]').should("exist")
+    cy.get('[data-testid="listing-filters"]').should("exist")
+    cy.get('[data-testid="listing-list"]').should("exist")
   })
 });
